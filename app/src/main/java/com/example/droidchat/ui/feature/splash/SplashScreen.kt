1661b0693b41c4537a5988a93b86505f1c73e082
Egito Filho <egitofilho4@gmail.com>
2024-10-19 20:30:01 -0300

    create a signIN screen

--- app/src/main/java/com/example/droidchat/ui/feature/splash/SplashScreen.kt
+++ app/src/main/java/com/example/droidchat/ui/feature/splash/SplashScreen.kt
@@ -10,10 +10,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -25,10 +25,18 @@ import androidx.compose.ui.unit.dp
 import com.example.droidchat.R
 import com.example.droidchat.ui.theme.BackgroundGradient
 import com.example.droidchat.ui.theme.DroidChatTheme
+import kotlinx.coroutines.delay
 
 @Composable
-fun SplashRoute() {
+fun SplashRoute(
+    onNavigateToSignIn: () -> Unit
+) {
     SplashScreen()
+
+    LaunchedEffect(Unit){
+        delay(2000)
+        onNavigateToSignIn()
+    }
 }
 
 @Composable
