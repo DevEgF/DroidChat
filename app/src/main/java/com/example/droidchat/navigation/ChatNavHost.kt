8c83eddfcf382518eba8682137ac07a43252eba0
Egito Filho <egitofilho4@gmail.com>
2025-01-08 18:13:48 -0300

    add animation

--- app/src/main/java/com/example/droidchat/navigation/ChatNavHost.kt
+++ app/src/main/java/com/example/droidchat/navigation/ChatNavHost.kt
@@ -1,10 +1,14 @@
 package com.example.droidchat.navigation
 
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.core.tween
 import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navOptions
+import com.example.droidchat.navigation.extension.slideInTo
+import com.example.droidchat.navigation.extension.slideOutTo
 import com.example.droidchat.ui.feature.signin.SignInRoute
 import com.example.droidchat.ui.feature.splash.SplashRoute
 
@@ -13,7 +17,18 @@ fun ChatNavHost() {
     val navController = rememberNavController()
 
     NavHost(navController, startDestination = SplashRoute) {
-        composable<SplashRoute> {
+        composable<SplashRoute>(
+            enterTransition =  {
+                this.slideInTo(
+                    direction = AnimatedContentTransitionScope.SlideDirection.Right,
+                )
+            },
+            exitTransition = {
+                this.slideOutTo(
+                    direction = AnimatedContentTransitionScope.SlideDirection.Left,
+                )
+            }
+        ) {
             SplashRoute(
                 onNavigateToSignIn = {
                     navController.navigate(
@@ -27,14 +42,36 @@ fun ChatNavHost() {
                 }
             )
         }
-        composable<SignInRoute> {
+        composable<SignInRoute>(
+            enterTransition =  {
+                this.slideInTo(
+                    direction = AnimatedContentTransitionScope.SlideDirection.Right,
+                )
+            },
+            exitTransition = {
+                this.slideOutTo(
+                    direction = AnimatedContentTransitionScope.SlideDirection.Left,
+                )
+            }
+        ) {
             SignInRoute(
                 navigateToSignUp = {
                     navController.navigate(SignUpRoute)
                 }
             )
         }
-        composable<SignUpRoute> {
+        composable<SignUpRoute>(
+            enterTransition =  {
+                this.slideInTo(
+                    direction = AnimatedContentTransitionScope.SlideDirection.Left,
+                )
+            },
+            exitTransition = {
+                this.slideOutTo(
+                    direction = AnimatedContentTransitionScope.SlideDirection.Left,
+                )
+            }
+        ) {
 
         }
     }
