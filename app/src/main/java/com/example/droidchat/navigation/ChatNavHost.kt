340d172ac1c042a22bce6ec9e8fb373ab37a6c8a
P0661708 <jose.egito@portoseguro.com.br>
2025-01-26 11:20:01 -0300

    empty space

--- app/src/main/java/com/example/droidchat/navigation/ChatNavHost.kt
+++ app/src/main/java/com/example/droidchat/navigation/ChatNavHost.kt
@@ -71,8 +71,6 @@ fun ChatNavHost() {
                     direction = AnimatedContentTransitionScope.SlideDirection.Left,
                 )
             }
-        ) {
-
-        }
+        ) {}
     }
 }
