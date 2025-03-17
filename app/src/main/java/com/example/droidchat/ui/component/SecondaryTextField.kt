319aad8a099e72d7aaed70d912bac8b7e40bb8c5
Egito Filho <egitofilho4@gmail.com>
2025-03-17 20:50:07 -0300

    criação de custom modifier com o API NODE

--- app/src/main/java/com/example/droidchat/ui/component/SecondaryTextField.kt
+++ app/src/main/java/com/example/droidchat/ui/component/SecondaryTextField.kt
@@ -1,6 +1,5 @@
 package com.example.droidchat.ui.component
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -12,12 +11,10 @@ import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
--- app/src/main/java/com/example/droidchat/ui/extensions/ModifierExtensions.kt
+++ app/src/main/java/com/example/droidchat/ui/extensions/ModifierExtensions.kt
@@ -4,6 +4,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.ContentDrawScope
+import androidx.compose.ui.node.DrawModifierNode
+import androidx.compose.ui.node.ModifierNodeElement
+import androidx.compose.ui.platform.InspectorInfo
 import androidx.compose.ui.unit.Dp
 
 fun Modifier.bottomBorder(color: Color, strokeWidth: Dp) = this.drawBehind {
@@ -19,3 +23,51 @@ fun Modifier.bottomBorder(color: Color, strokeWidth: Dp) = this.drawBehind {
         strokeWidth = strokeWidthPx
     )
 }
+
+/* Utilizando a API .NODE */
+private class BottomBorderNode(
+    var color: Color,
+    var strokeWidth: Dp,
+): DrawModifierNode, Modifier.Node() {
+
+    override fun ContentDrawScope.draw() {
+        val strokeWidthPx = strokeWidth.toPx()
+
+        val width = size.width
+        val height = size.height
+
+        drawLine(
+            color = color,
+            start = Offset(0f, height - strokeWidthPx),
+            end = Offset(width, height - strokeWidthPx),
+            strokeWidth = strokeWidthPx
+        )
+    }
+}
+
+private data class BottomBorderElement(
+    val color: Color,
+    val strokeWidth: Dp,
+): ModifierNodeElement<BottomBorderNode>() {
+
+    override fun create(): BottomBorderNode {
+        return BottomBorderNode(
+            color = color,
+            strokeWidth = strokeWidth
+        )
+    }
+
+    override fun update(node: BottomBorderNode) {
+        node.color = color
+        node.strokeWidth = strokeWidth
+    }
+
+    override fun InspectorInfo.inspectableProperties() {
+        name = "bottomBorder"
+        properties["color"] = color
+        properties["strokeWidth"] = strokeWidth
+    }
+}
+
+fun Modifier.bottomBorder2(color: Color, strokeWidth: Dp) =
+    this then BottomBorderElement(color, strokeWidth)
