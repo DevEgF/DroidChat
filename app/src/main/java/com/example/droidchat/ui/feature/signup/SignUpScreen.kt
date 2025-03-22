36952ce50047bfd55e0d2ed1c3489f8d1c0935b9
Egito Filho <egitofilho4@gmail.com>
2025-03-22 10:34:31 -0300

    create modal bottom sheet

--- app/src/main/java/com/example/droidchat/ui/feature/signup/SignUpScreen.kt
+++ app/src/main/java/com/example/droidchat/ui/feature/signup/SignUpScreen.kt
@@ -1,8 +1,9 @@
 package com.example.droidchat.ui.feature.signup
 
+import android.net.Uri
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.rememberScrollableState
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -12,9 +13,14 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -25,16 +31,18 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.droidchat.R
 import com.example.droidchat.ui.component.PrimaryButton
+import com.example.droidchat.ui.component.ProfilePictureOptionsModalBottomSheet
+import com.example.droidchat.ui.component.ProfilePictureSelector
 import com.example.droidchat.ui.component.SecondaryTextField
 import com.example.droidchat.ui.theme.BackgroundGradient
 import com.example.droidchat.ui.theme.DroidChatTheme
-import com.example.droidchat.ui.theme.Surface
 
 @Composable
 fun SignUpRouteRoute() {
     SignUpRouteScreen()
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SignUpRouteScreen() {
     Box(
@@ -48,6 +56,15 @@ fun SignUpRouteScreen() {
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
             Spacer(Modifier.height(56.dp))
+
+            var profilePictureSelectedUri by remember {
+                mutableStateOf<Uri?>(null)
+            }
+
+            var openProfilePictureOptionsModelBottomSheet by remember {
+                mutableStateOf(false)
+            }
+
             Image(
                 painter = painterResource(id = R.drawable.logo),
                 contentDescription = null
@@ -62,8 +79,16 @@ fun SignUpRouteScreen() {
                 color = MaterialTheme.colorScheme.surface
             ) {
                 Column(
-                    modifier = Modifier.padding(16.dp)
+                    modifier = Modifier.padding(16.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
                 ) {
+                    ProfilePictureSelector(
+                        imageUri = profilePictureSelectedUri,
+                        modifier = Modifier.clickable {
+                            openProfilePictureOptionsModelBottomSheet = true
+                        }
+                    )
+                    Spacer(Modifier.height(30.dp))
                     SecondaryTextField(
                         label = stringResource(R.string.feature_sign_up_first_name),
                         value = "",
@@ -103,6 +128,12 @@ fun SignUpRouteScreen() {
                         onClick = {}
                     )
                 }
+            }
+
+            if (openProfilePictureOptionsModelBottomSheet) {
+                ProfilePictureOptionsModalBottomSheet(onDismissRequest = {
+                    openProfilePictureOptionsModelBottomSheet = false
+                })
             }
         }
     }
